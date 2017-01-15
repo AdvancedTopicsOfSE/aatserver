af32715e5b8c2b498589fb35f96615f400496d23
dorelcomanj@gmail.com <dorel.coman@tum.de>
2017-01-15 15:15:36 +0100

    initial commit

--- main.py
+++ main.py
@@ -11,6 +11,11 @@ def hello():
     """Return a friendly HTTP greeting."""
     return 'Hello World!'
 
+@app.route('/login')
+def hello():
+    """Return a friendly HTTP greeting."""
+    return 'LOGIN!'
+
 
 @app.errorhandler(404)
 def page_not_found(e):
